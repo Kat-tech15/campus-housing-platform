254d6678b86f2bb63f0a666d03a56ed6a229347d
Kat-tech15 <kelvinkatwai@gmail.com>
2026-03-26 17:00:48 +0300

    modified settings

--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,16 @@ from rest_framework.response import Response
 from .serializers import ProfileSerializer, ProfileSerializer, UserSerializer, VerifyEmailSerializer, LoginSerializer, EmptySerializer
 from .models import CustomUser, Profile
 from django.core.mail import send_mail
+from drf_spectacular.utils import extend_schema
 from django.conf import settings
 from django.contrib.auth import authenticate    
 
 
+@extend_schema(tags=['Accounts'])
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
     queryset = CustomUser.objects.all()
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -27,9 +30,10 @@ class RegisterView(generics.CreateAPIView):
     
         return Response({"message": "User registered successfully. Please check your email to verify your account."}, status=status.HTTP_201_CREATED)
     
-
+@extend_schema(tags=['Accounts'])
 class VerifyEmailView(generics.GenericAPIView):
     serializer_class = VerifyEmailSerializer
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -44,9 +48,11 @@ class VerifyEmailView(generics.GenericAPIView):
             return Response({"message": "Email verified successfully. You can now log in."}, status=status.HTTP_200_OK)
         except CustomUser.DoesNotExist:
             return Response({"error": "Invalid verification code."}, status=status.HTTP_400_BAD_REQUEST)
-        
+
+@extend_schema(tags=['Accounts'])      
 class LoginView(generics.CreateAPIView):
     serializer_class = LoginSerializer
+    permission_classes = [permissions.AllowAny]
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -63,14 +69,18 @@ class LoginView(generics.CreateAPIView):
                 return Response({"error": "Email not verified. Please verify your email before logging in."}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({"error": "Invalid username or password."}, status=status.HTTP_400_BAD_REQUEST)
-        
+
+  
+@extend_schema(tags=['Accounts'])
 class LogoutView(generics.CreateAPIView):
     serializer_class = EmptySerializer
+    permission_classes = [permissions.IsAuthenticated]  
 
     def post(self, request, *args, **kwargs):
         return Response({"message": "Logout successful."}, status=status.HTTP_200_OK)
     
 
+@extend_schema(tags=['Accounts'])
 class UpdateProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
